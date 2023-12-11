9cb093a5921c6d54e6ecc6cbe589c2fa08c1242a
jorginho222 <rodizioperalta22@gmail.com>
2023-12-11 15:48:14 -0300

    Get movies with filters. Create movie validating with zod

--- parte3/app.js
+++ parte3/app.js
@@ -1,10 +1,48 @@
 const express = require('express')
+const crypto = require('node:crypto')
+const movies = require('./movies.json')
+const { validateMovie } = require('./schemas/movies')
 
 const app = express()
 app.disable('x-powered-by')
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ message: 'holu' })
+})
+
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const byGenre = movies.filter(movie => movie.genre.some(movieGenre => movieGenre.toLowerCase() === genre.toLowerCase()))
+    if (byGenre.length > 0) return res.json(byGenre)
+    res.status(404).json({ message: `No Movie with genre ${genre} was found` })
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const validation = validateMovie(req.body)
+
+  if (validation.error) {
+    return res.status(400).json({ error: JSON.parse(validation.error.message) })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...validation.data
+  }
+
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
 })
 
 const PORT = process.env.PORT ?? 1234
---
+++ parte3/schemas/movies.js
@@ -0,0 +1,39 @@
+const z = require('zod')
+
+const moviesSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Title must be a string',
+    required_error: 'Movie title is required'
+  }),
+  year: z.number().int().positive().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL'
+  }),
+  genre: z.array(
+    z.enum([
+      'Animation',
+      'Adventure',
+      'Drama',
+      'Sci-Fi',
+      'Crime',
+      'Romance',
+      'Biography',
+      'Fantasy',
+      'Thriller'
+    ], {
+      required_error: 'Movie genre is required',
+      invalid_type_error: 'Movie genre must be one of the genres list'
+    })
+  )
+})
+
+function validateMovie (object) {
+  return moviesSchema.safeParse(object)
+}
+
+module.exports = {
+  validateMovie
+}
